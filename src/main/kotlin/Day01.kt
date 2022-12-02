79b4d8fab07ff0640dac0a52d8b66ffd18ba81da
pedroMalaf <pedrid02@gmail.com>
2022-12-02 23:16:14 +0000

    day01 part2 implemented

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -21,5 +21,21 @@ fun part1(){
 }
 
 fun part2(){
-
+    val input = readInput("day01")
+    val caloriesList = mutableListOf<Int>()
+    var sum = 0
+    var sumOfTop3 = 0
+    input.forEach { calories ->
+        if (calories.isNotEmpty())
+            sum += calories.toInt()
+        if (calories.isEmpty()) {
+            caloriesList.add(sum)
+            sum = 0
+        }
+    }
+    val sortedList = caloriesList.sortedByDescending { it }
+    for (i in 0..2){
+        sumOfTop3 += sortedList[i]
+    }
+    println(sumOfTop3)
 }
