d80e8a98f69ee141e7aacced3d5e93192732603b
pedroMalaf <pedrid02@gmail.com>
2023-11-02 15:12:22 +0000

    Day 2 done

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -6,6 +6,7 @@ const val DRAW_VALUE = 3
 const val LOSS_VALUE = 0
 fun main(){
     println(day2Part1())
+    println(day2Part2())
 }
 
 fun day2Part1(): Int{
@@ -33,6 +34,28 @@ fun day2Part1(): Int{
     return totalScore
 }
 
-fun day2Part2(){
-    TODO()
+fun day2Part2(): Int{
+    val lines = readInput("day02")
+    var totalScore = 0
+    lines.forEach { line ->
+        when(line[0]){
+            'A' -> {
+                if (line[2] == 'X') totalScore += LOSS_VALUE + SCISSORS_VALUE
+                if (line[2] == 'Y') totalScore += DRAW_VALUE + ROCK_VALUE
+                if (line[2] == 'Z') totalScore += WIN_VALUE + PAPER_VALUE
+            }
+            'B' -> {
+                if (line[2] == 'X') totalScore += LOSS_VALUE + ROCK_VALUE
+                if (line[2] == 'Y') totalScore += DRAW_VALUE + PAPER_VALUE
+                if (line[2] == 'Z') totalScore += WIN_VALUE + SCISSORS_VALUE
+
+            }
+            else -> {
+                if (line[2] == 'X') totalScore += LOSS_VALUE + PAPER_VALUE
+                if (line[2] == 'Y') totalScore += DRAW_VALUE + SCISSORS_VALUE
+                if (line[2] == 'Z') totalScore += WIN_VALUE + ROCK_VALUE
+            }
+        }
+    }
+    return totalScore
 }
